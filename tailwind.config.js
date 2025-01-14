edbf683bea12db79a6e6cfe6528dfdc45768b1de
faensi <knobling.hubertus@gmail.com>
2025-01-14 13:01:45 +0100

    change layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        metalmania: ['Metalmania', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
